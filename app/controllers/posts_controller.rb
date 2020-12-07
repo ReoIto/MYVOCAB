5ae4b08cc3cb56731d2456665ba75f9e3bf2e5c6
ReoIto <leo.0210.ito@gmail.com>
2020-12-07 16:14:49 +0900

    First commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+    @id = params[:id]
+    @post = Post.find_by(id: params[:id] )
+  end
+
+  def new
+
+  end
+
+  def create
+    @post = Post.new(content: params[:content], meaning: params[:meaning], example: params[:example], synonyms: params[:synonyms], antonyms: params[:antonyms], note: params[:note])
+    if @post.save
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id] )
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content  = params[:content]
+    @post.meaning  = params[:meaning]
+    @post.example  = params[:example]
+    @post.synonyms = params[:synonyms]
+    @post.antonyms = params[:antonyms]
+    @post.note     = params[:note]
+    if @post.save
+      flash[:notice] = "The list has edited."
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+  end
+end
