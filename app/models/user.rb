1fc33c059f33b5ffa8eabfab0ff332d8f44e8413
ReoIto <leo.0210.ito@gmail.com>
2020-12-10 10:41:08 +0900

    Created LOGIN & LOGOUT function.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
-  validates :name, {presence: true, length: {maximum: 100}}
-  validates :email, {presence: true, uniqueness: true, length: {maximum: 100}}
+  validates :name, {
+                    presence: true,
+                    length: {maximum: 100}
+                   }
+
+  validates :email, {
+                     presence: true,
+                     uniqueness: true,
+                     length: {maximum: 100}
+                    }
+
+  validates :password, {
+                        presence: true
+                       }
+
 end
