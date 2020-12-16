a419e4929b329bf136d023c998b3bfbc39a47f45
ReoIto <leo.0210.ito@gmail.com>
2020-12-16 11:02:08 +0900

    Restricted permissions to edit other user's posts by before_action.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
-
+  before_action :ensure_current_user, {only: [:edit, :update, :destroy]}
 
   def index
     @posts = Post.all.order(created_at: :desc)
@@ -63,5 +63,13 @@ class PostsController < ApplicationController
     @post.destroy
     flash[:notice] = "the list has been deleted."
     redirect_to("/posts/index")
+  end
+
+  def ensure_current_user
+    @post = Post.find_by(id: params[:id])
+      if @post.user_id != @current_user.id
+        flash[:notice] = "You don't have the authority to."
+        redirect_to("/posts/index")
+      end
   end
 end
