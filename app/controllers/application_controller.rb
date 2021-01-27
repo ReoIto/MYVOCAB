ac87845aba8a726c05158372b3bec3725242babe
ReoIto <leo.0210.ito@gmail.com>
2021-01-27 12:13:49 +0900

    errors: Added authenticate_user mathod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :set_search
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, {only: [:index, :show, :edit, :update]}
 
   def set_search
     @search = Post.ransack(params[:q])
@@ -15,18 +16,26 @@ class ApplicationController < ActionController::Base
     @current_user = User.find_by(id: session[:user_id])
   end
 
-  # def
-  #   if @current_user == nil
-  #     flash[:notice] = "Please login to your account."
-  #     redirect_to("/login")
-  #   end
-  # end
+  # def authenticate_user
+  #  if @current_user == nil
+  #    flash[:notice] = "Please login to your account."
+  #    redirect_to("/login")
+  #  end
+  #end
 
   def forbid_login_user
-    if @current_user
+    if current_user
       flash[:notice] = "Your account is already logged in."
       redirect_to("/posts/index")
     end
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    top_path
   end
 
   protected
