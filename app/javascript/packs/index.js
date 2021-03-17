e0790e343c234cf4762d47fc326924ab4ee537b7
ReoIto <leo.0210.ito@gmail.com>
2021-03-17 10:14:17 +0900

    Add JQuery through yurn

---
+++ app/javascript/packs/index.js
@@ -0,0 +1,18 @@
+$(function(){
+console.log('★★★★')
+});
+
+
+// $(document).on('turbolinks:load', function () {
+//   if ($("nav ul.pagination a[rel=next]").length) {
+//     $('.talk-cards').infiniteScroll({
+//       path: "nav ul.pagination a[rel=next]",
+//       append: ".talk-cards .card-index",
+//       hideNav: "nav ul.pagination",
+//       history: false,
+//       scrollThreshold: false,
+//       button: ".loadmore-btn",
+//       status: ".page-load-status",
+//     });
+//   };
+// });
